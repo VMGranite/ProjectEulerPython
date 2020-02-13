67e8549e7c6d1b1db601eb69c9325a0367e9bc9f
Veronica <VMGranite@gmail.com>
2020-02-12 20:28:21 -0500

    https://projecteuler.net/problem=13

---
+++ 013_Large_Sum.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# 2/13/2020
+# PROJECT EULER 
+# Large Sum: Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+# https://projecteuler.net/problem=13
+
+sum = 0
+f = '50DNums.txt'
+with open(f) as file:
+   for line in file:
+        sum += int(line)
+
+
+sumString = str(sum)
+print("Sum: " + sumString)
+print("First 10: " + sumString[0:10])
+
